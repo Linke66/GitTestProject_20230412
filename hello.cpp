f0701b6b7b2994573f219b303ada7ade13cbe7a5
Mo Yongchao <1220276266@qq.com>
2023-04-20 16:26:37 +0800

    Add the function of adding two numbers.(add three files:add.h,add.cpp,hello)

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+#include "add.h"
+
 int main(){
-  std::cout << "hello world" << std::endl;
+  int a, b;
+  std::cout << "Please enter the first number:";
+  std::cin >> a;
+
+  std::cout << "Please enter the second number:";
+  std::cin >> b;
+
+  std::cout << "The result of adding two numbers is:" << MyAdd(a, b) << std::endl;
   std::cout << "hello world" << std::endl;
 
   return 0;
